224cb563b9ef504f115bece747d14f34279f2dec
take_cheeze <takechi101010@gmail.com>
2013-08-24 15:35:28 +0900

    store ModuleInternal to module Player

--- src/player.cpp
+++ src/player.cpp
@@ -63,7 +63,8 @@ mrb_value get_player(mrb_state* M) {
 
 ModuleInternal& internal(mrb_state* M) {
 	M = get_vm(M);
-	return EasyRPG::get<ModuleInternal>(M, mrb_vm_cv_get(M, mrb_intern(M, "_module_internal")));
+	return EasyRPG::get<ModuleInternal>(
+		M, mrb_mod_cv_get(M, mrb_class_get(M, "Player"), mrb_intern(M, "_module_internal")));
 }
 
 }
@@ -83,7 +84,7 @@ mrb_state* Player::create_vm() {
 	assert(data);
 	new(ptr) ModuleInternal(M);
 
-	mrb_vm_cv_set(M, mrb_intern(M, "_module_internal"), mrb_obj_value(data));
+	mrb_mod_cv_set(M, mrb_class_get(M, "Player"), mrb_intern(M, "_module_internal"), mrb_obj_value(data));
 #ifdef NDEBUG
 	mrb_gv_set(M, mrb_intern(M, "$DEBUG"), mrb_false_value());
 #else
