3f420bf9674355404c174d3cbe089858e077b437
take_cheeze <takechi101010@gmail.com>
2013-09-04 15:46:28 +0900

    use rake instead of minirake in build script

--- build_mruby.sh
+++ build_mruby.sh
@@ -7,4 +7,6 @@ if [ ! -d $SCRIPT_PATH/mruby ] ; then
 		git clone git@github.com:mruby/mruby.git $SCRIPT_PATH/mruby
 fi
 
-MRUBY_CONFIG=$SCRIPT_PATH/build_config.rb make -C $SCRIPT_PATH/mruby $@
+cd $SCRIPT_PATH/mruby
+
+MRUBY_CONFIG=$SCRIPT_PATH/build_config.rb rake $@
