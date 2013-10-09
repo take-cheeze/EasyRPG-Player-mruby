7095c7967314b243b4ad315b917724c56fe9ee9d
take_cheeze <takechi101010@gmail.com>
2013-10-09 23:37:57 +0900

    add .travis.yml

--- build_mruby.sh
+++ build_mruby.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash
 
 cd $(dirname $0)
 SCRIPT_PATH=$(pwd)
 
 if [ ! -d $SCRIPT_PATH/mruby ] ; then
-		git clone --depth 1 git@github.com:EasyRPG/mruby.git $SCRIPT_PATH/mruby
+		git clone --depth 1 https://github.com/EasyRPG/mruby.git $SCRIPT_PATH/mruby
 fi
 
 cd $SCRIPT_PATH/mruby
