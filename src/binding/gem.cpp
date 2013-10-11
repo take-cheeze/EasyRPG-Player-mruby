06d1c0e93bcad30a98bb7196cba29c7ff45d6fa2
take_cheeze <takechi101010@gmail.com>
2013-10-11 14:19:40 +0900

    support block passing in Array's index and rindex

--- src/binding/gem.cpp
+++ src/binding/gem.cpp
@@ -1,5 +1,53 @@
 #include "binding.hxx"
 
+namespace {
+
+mrb_value array_index(mrb_state* M, mrb_value const self) {
+	mrb_value obj = mrb_undef_value();
+	
+	mrb_get_args(M, "|o", &obj);
+	if(not mrb_undef_p(obj)) {
+		for(mrb_int i = 0; i < RARRAY_LEN(self); ++i) {
+			if(mrb_equal(M, RARRAY_PTR(self)[i], obj)) {
+				return mrb_fixnum_value(i);
+			}
+		}
+	} else {
+		mrb_get_args(M, "&", &obj);
+		for(mrb_int i = 0; i < RARRAY_LEN(self); ++i) {
+			if(mrb_test(mrb_yield(M, obj, RARRAY_PTR(self)[i]))) {
+				return mrb_fixnum_value(i);
+			}
+		}
+	}
+
+	return mrb_nil_value();
+}
+
+mrb_value array_rindex(mrb_state* M, mrb_value const self) {
+	mrb_value obj = mrb_undef_value();
+	
+	mrb_get_args(M, "|o", &obj);
+	if(not mrb_undef_p(obj)) {
+		for(mrb_int i = RARRAY_LEN(self) - 1; i >= 0; --i) {
+			if(mrb_equal(M, RARRAY_PTR(self)[i], obj)) {
+				return mrb_fixnum_value(i);
+			}
+		}
+	} else {
+		mrb_get_args(M, "&", &obj);
+		for(mrb_int i = RARRAY_LEN(self) - 1; i >= 0; --i) {
+			if(mrb_test(mrb_yield(M, obj, RARRAY_PTR(self)[i]))) {
+				return mrb_fixnum_value(i);
+			}
+		}
+	}
+
+	return mrb_nil_value();
+}
+
+}
+
 extern "C" void mrb_EasyRPG_Player_gem_init(mrb_state* M) {
 	EasyRPG::register_audio(M);
 	EasyRPG::register_bitmap(M);
@@ -20,6 +68,9 @@ extern "C" void mrb_EasyRPG_Player_gem_init(mrb_state* M) {
 	EasyRPG::register_tone(M);
 	EasyRPG::register_utils(M);
 	EasyRPG::register_window(M);
+
+	mrb_define_method(M, M->array_class, "index", &array_index, MRB_ARGS_OPT(1) | MRB_ARGS_BLOCK());
+	mrb_define_method(M, M->array_class, "rindex", &array_rindex, MRB_ARGS_OPT(1) | MRB_ARGS_BLOCK());
 }
 
 extern "C" void mrb_EasyRPG_Player_gem_final(mrb_state*) {}
