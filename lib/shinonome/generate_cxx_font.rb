e1392cfb360b5be70baa9934eb1724a0e7f9150d
take_cheeze <takechi101010@gmail.com>
2013-10-11 23:36:13 +0900

    refactor font generating script and install iconv gem

--- lib/shinonome/generate_cxx_font.rb
+++ lib/shinonome/generate_cxx_font.rb
@@ -4,17 +4,17 @@
 FONT_SIZE = 12
 EMPTY_CHAR = Array.new(FONT_SIZE, 0x0)
 
-kanji_encoding = "JIS_X0208"
+KanjiEncoding = "JIS_X0208"
+HankakuEncoding = "JIS_X0201"
 
+require 'rubygems'
 require 'iconv'
 
 def skip_until(f, regex)
   while(not f.eof?)
     l = f.readline().chomp
     ret = regex.match(l)
-    if ret
-      return ret
-    end
+    return ret if ret
   end
   return false
 end
@@ -35,74 +35,63 @@ end
 
 def read_font(f, half, encoding)
   code = skip_until(f, /STARTCHAR\s+(\w+)/)
-  if not code
-    return false
-  else
-    c = code[1].hex
-    if encoding == "JIS_X0208"
-      encoding = "CP932"
-
-      j1 = (c >> 8) & 0xff
-      j2 = c & 0xff
-
-      s1 = (32 < j1 and j1 <= 94) ? (j1 + 1) / 2 + 112 : (j1 + 1) / 2 + 176
-      s2 = (j1 & 0x01) == 1 ? j2 + 31 + j2 / 96 : j2 + 126
-
-      c = ((s1 & 0xff) << 8) + s2
-      code = [s1, s2].pack("CC")
-    else
-      code = (c < 0x100 ? [c] : [c & 0xff, (c >> 8) & 0xff]).pack("C*")
-    end
-
-    raise "size error" unless code.bytesize == (c < 0x100 ? 1 : 2)
-
-    begin
-      # p Iconv.conv("UTF-8", encoding, code)
-      code = Iconv.conv('UTF-32LE', encoding, code)
-    rescue Iconv::IllegalSequence
-      print "invalid code 0x%02x, 0x%02x\n" % [(c >> 8) & 0xff, c & 0xff]
-      return false
-    end
-
-    raise "invalid code" unless code.bytesize == 4
-
-    code = code.unpack("V")[0]
-  end
-
-  width = half ? FONT_SIZE / 2 : FONT_SIZE
+  return nil if not code
 
   skip_until(f, /BITMAP/)
-
   ret = Array.new(FONT_SIZE, 0)
-
   for y in 0...FONT_SIZE
-    str = f.readline()
+    str = f.readline
 
     tmp = 0
-    for x in 0...width
-      if str[x] == ?@
-        tmp |= 0x01 << x
-      else
-        raise "assert" unless str[x] == ?.
+    for x in 0...(half ? FONT_SIZE / 2 : FONT_SIZE)
+      if str[x] == ?@;tmp |= 0x01 << x
+      else; raise "unknown dot symbol: #{str[x]}" unless str[x] == ?.
       end
     end
     ret[y] = tmp
   end
+  raise 'cannot end char' unless /ENDCHAR/.match(Iconv.conv('UTF-8', 'EUC-JP', f.readline()))
 
-  raise "assert" unless /ENDCHAR/.match(Iconv.conv('UTF-8', 'EUC-JP', f.readline()))
+  c = code[1].hex
+  if encoding == KanjiEncoding
+    encoding = "CP932"
 
-  return Glyph.new(code, !half, ret)
+    j1 = (c >> 8) & 0xff
+    j2 = c & 0xff
+
+    s1 = (32 < j1 and j1 <= 94) ? (j1 + 1) / 2 + 112 : (j1 + 1) / 2 + 176
+    s2 = (j1 & 0x01) == 1 ? j2 + 31 + j2 / 96 : j2 + 126
+
+    c = ((s1 & 0xff) << 8) + s2
+    code = [s1, s2].pack("CC")
+  elsif encoding ==  HankakuEncoding
+    raise "too big code: #{c}" unless c < 0x100
+    return nil if 0xE0 <= c
+    code = [c].pack('C')
+    encoding = 'CP932'
+  else
+    code = (c < 0x100 ? [c] : [c & 0xff, (c >> 8) & 0xff]).pack("C*")
+  end
+
+  raise "size error" unless code.bytesize == (c < 0x100 ? 1 : 2)
+
+  begin; code = Iconv.conv('UTF-32LE', encoding, code)
+  rescue Iconv::IllegalSequence
+    print "invalid code 0x%02x, 0x%02x\n" % [(c >> 8) & 0xff, c & 0xff]
+    return false
+  end
+
+  raise "invalid code: #{c} #{code.bytes.to_a}" unless code.bytesize == 4
+
+  return Glyph.new(code.unpack("V")[0], !half, ret)
 end
 
 def read_file(f, encoding, half)
   ret = {}
   while(true)
     font = read_font(f, half, encoding)
-    if f.eof?
-      return ret
-    elsif font
-      ret[font.code] = font
-    end
+    ret[font.code] = font unless font.nil?
+    return ret if f.eof?
   end
 end
 
@@ -131,20 +120,20 @@ latin = read_file(File.new('./latin1/font_src.bit', 'r'), "latin1", true)
 print "done\n"
 
 print "Loading Hankaku..."
-hankaku = read_file(File.new('./hankaku/font_src_diff.bit', 'r'), "JIS_X0201", true)
+hankaku = read_file(File.new('./hankaku/font_src_diff.bit', 'r'), HankakuEncoding, true)
 print "done\n"
 
 print "Loading Gothic..."
-gothic = read_file(File.new('./kanjic/font_src.bit', 'r'), kanji_encoding, false)
+gothic = read_file(File.new('./kanjic/font_src.bit', 'r'), KanjiEncoding, false)
 print "done\n"
 
 print "Loading Mincho..."
-mincho = read_file(File.new('./mincho/font_src_diff.bit', 'r'), kanji_encoding, false)
+mincho = read_file(File.new('./mincho/font_src_diff.bit', 'r'), KanjiEncoding, false)
 print "done\n"
 
 # generating
 print "Generating Gothic..."
-gothic_final = gothic.merge(hankaku).merge(latin)
+gothic_final = gothic.merge(hankaku).merge!(latin)
 code_max = write_all(File.new("./src/gothic.cxx", "w"), "SHINONOME_GOTHIC", gothic_final)
 print "done\n"
 
